0605fb6d9f494a46d3908b5e487bb95a0083cc9c
Anderson Sousa <116604139+AndSunN@users.noreply.github.com>
2022-11-06 22:02:00 -0300

    Create inicio.php

---
+++ inicio.php
@@ -0,0 +1,93 @@
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class="logocopa" src="https://upload.wikimedia.org/wikipedia/pt/thumb/e/e3/2022_FIFA_World_Cup.svg/1200px-2022_FIFA_World_Cup.svg.png" alt="copinha"> </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/projeto1/?#%22%3EInício"</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="inicio.php">INICIO</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="formulario.php">JOGADORES</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="pesquisar.php">PESQUISAR</a>
+                  </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+
+
+      <div class="textocentrocontainer">
+          <h2 class="titulo">Copa do Mundo Fifa 2022</h2>
+      </div>
+
+
+<div class="containerrato">
+    <div class="titulo2">Campeonato Mundial de Futebol FIFA de 2022 <br>
+    </div>
+    
+ 
+<div class="lista">
+    <img class="imagem" src="https://placar.abril.com.br/wp-content/uploads/2022/01/Mundial-de-clubes-1.jpg?quality=70&strip=info&resize=680,453" alt="">
+
+    <p class="escrita">
+      A Copa do Mundo FIFA de 2022 ou Campeonato Mundial de Futebol FIFA de 2022 será a vigésima segunda edição deste evento esportivo, um torneio internacional de futebol masculino organizado pela Federação Internacional de Futebol (FIFA), que ocorrerá no Catar. Com sete cidades-sede, o campeonato será disputado entre 21 de novembro e 18 de dezembro, primeira vez em que é disputado no final do ano.[1][2] A edição de 2022 será a primeira realizada no Oriente Médio e a última a ter o formato de 32 equipes, já que a competição terá uma mudança no formato e número de equipes na edição de 2026, onde será sediado no Canadá, Estados Unidos e México, passando para 48 equipes.
+      <p class="escrita">
+Acusações de corrupção foram feitas após o Catar ganhar o direito de sediar o campeonato. A FIFA realizou uma investigação interna sobre estas alegações e absolveu o Catar de qualquer crime cometido.[3] Em 27 de maio de 2015, promotores federais suíços abriram uma investigação sobre corrupção e lavagem de dinheiro nas eleições das cidades-sede das edições de 2018 e 2022.[4][5]
+</p>
+<p class="escrita">
+Em 7 de junho de 2015, foi anunciado que o Catar poderia perder o direito de sediar o campeonato, após denúncias de suborno. De acordo com Domenico Scala, representante da auditoria da FIFA, as denúncias surgiram após a divulgação do resultado da votação de eleição da cidade-sede.[6] Porém, nenhum indício foi comprovado e o Catar foi confirmado como sede desta edição.[7]
+</p>
+<p class="escrita">
+O Catar sofreu diversas criticas sobre as condições dos trabalhadores dos novos estádios para a competição, sendo que a Anistia Internacional referiu-se como trabalho escravo as condições dos trabalhadores, que sofriam abusos de direitos humanos, violando diversas regras da instituição.[8]
+    </p>
+
+
+    <img class="imagem" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Map_of_2018_FIFA_World_Cup_bids.svg/400px-Map_of_2018_FIFA_World_Cup_bids.svg.png" alt="">
+
+    <div class="titulo2">Candidatura <br>
+    </div>
+
+    <p class="escrita">
+    O Catar decidiu apresentar a sua candidatura apenas para 2022. Com apenas 1,6 milhão de habitantes, o país pretendeu ser a primeira nação do Oriente Médio a receber tal competição, tentando aproximar os mundos ocidentais e orientais.[9][10]
+  </p>
+
+  <p class="escrita">
+  Uma das preocupações da candidatura foram as temperaturas muito elevadas que se registram naquele país. A Copa do Mundo sempre foi realizada entre junho e julho, exatamente o período de recesso das ligas europeias. Nesse período de tempo, as temperaturas ultrapassam facilmente os 40 graus celsius, nunca baixando para menos de 30 graus celsius.[11][12] O sheik Mohammed bin Hamad bin Khalifa al-Thani, autoridade responsável pela candidatura de 2022, afirmou que está em curso a introdução de uma tecnologia dentro dos estádios de modo a conseguir baixar as temperaturas em cerca de 20 graus celsius.[13]
+</p>
+
+
+<p class="escrita">
+O então presidente da FIFA, na epoca Joseph Blatter, admitiu apoiar a ideia de um país do Oriente Médio recebesse a competição. Ele afirmou que a região merece receber tal competição, já que 22 países da região nunca tiveram uma oportunidade concreta de receber um torneio deste tamanho. Blatter ainda afirma que ficou surpreendido com a evolução desta candidatura e reiterou que o país já mostrou ser capaz de organizar tal competição, quando sediou com sucesso os Jogos Asiáticos de 2006.[14]
+</p>
+
+
+<div class="titulo2">Candidatura <br>
+</div>
+
+
+<p class="escrita">
+O processo de escolha para as sedes das edições de 2018 e 2022 iniciou em janeiro de 2009, e as associações interessadas tinham até 2 de fevereiro de 2009 para enviar a documentação necessária.[15] Inicialmente, onze propostas foram recebidas pela FIFA, porém o México decidiu desistir do processo,[16][17] e a candidatura da Indonésia foi rejeitada pela FIFA em fevereiro de 2010, após a Associação de Futebol da Indonésia não apresentar uma carta de garantia do governo indonésio para apoiar a candidatura.[18]</p>
+<p class="escrita">
+
+No final, havia cinco propostas para a competição, a Austrália, Catar, Coreia do Sul, Estados Unidos e Japão. Os membros do Comitê Executivo da FIFA se reuniram em Zurique em 2 de dezembro de 2010 para votar e selecionar as sedes das duas edições.[19] Dois membros foram suspensos antes da votação após alegações de corrupção em relação aos seus votos.[20]</p>
+</div>
+
+    </h2>
+
+</main>
+</body>
+</html>
